7b198f77feeb7bcc60b3851e0968cac2ca68e858
lpac21 <116370454+lpac21@users.noreply.github.com>
2025-05-30 10:12:26 +1000

    Profile page w grid no scroll

--- saa-app/src/home/profile/index.tsx
+++ saa-app/src/home/profile/index.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import SideBar from "../../components/sideBar";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMapMarkerAlt} from "@fortawesome/free-solid-svg-icons";
+import SimpleCourseCard from "../../components/simpleCourseCard/simpleCourseCard";
+import SavedUniversity from "../../components/savedUniversity";
 
 function Profile() {
     return (
-        <SideBar accountName="John Doe"
-                 accountMajor="Computer Science" />
+        <div className = "side-by-side">
+            <div className = "side-bar">
+                <SideBar accountName="John Doe"
+                         accountMajor="Computer Science" />
+            </div>
+            <div className = "rest-of-page">
+                <span className="section-title">My Universities</span>
+                <div className = "university-div">
+                    <div className="university-row">
+                        <SavedUniversity universityName={"University of Technology Sydney"}
+                                         coursesSaved={4} loc={"Sydney, AU"}/>
+                        <SavedUniversity universityName={"IES Barcelona"}
+                                         coursesSaved={2} loc={"Barcelona, ES"}/>
+                        <SavedUniversity universityName={"DIS Cophenhagen"}
+                                         coursesSaved={2} loc={"Cophenhagen, DE"}/>
+                        <SavedUniversity universityName={"DIS Cophenhagen"}
+                                         coursesSaved={2} loc={"Cophenhagen, DE"}/>
+                        <SavedUniversity universityName={"IES Barcelona"}
+                                         coursesSaved={2} loc={"Barcelona, ES"}/>
+                    </div>
+                </div>
+                <div className = "all-courses">
+                    <span className = "section-title">Recently Viewed Courses</span>
+                    <div className="course-grid">
+                        <SimpleCourseCard courseNumber='CS3500' />
+                        <SimpleCourseCard courseNumber='CS3000' />
+                        <SimpleCourseCard courseNumber='CS3800' />
+                    </div>
+                </div>
+
+            </div>
+        </div>
     );
 }
 export default Profile;
+
+/*
+*/
