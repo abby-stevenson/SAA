46a35b2f9bde2bdddfcfa3484e776842277769bc
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2025-06-02 09:07:11 +1000

    Added Sydney Photos and small style changes

--- saa-app/src/components/studentTestimonials/studentTestimonials.tsx
+++ saa-app/src/components/studentTestimonials/studentTestimonials.tsx
@@ -1,39 +1,55 @@
 import React from 'react';
 import './studentTestimonials.css';
 
-const StudentTestimonials = () => {
+interface StudentTestimonialsProps {
+  name: string;
+  story: string;
+  mainImage: string;
+  image1: string;
+  image2: string;
+  image3: string;
+  image4: string;
+  image5: string;
+  image6: string;
+}
+
+const StudentTestimonials = ({name, story, mainImage, image1, image2, image3, image4, image5, image6}: StudentTestimonialsProps) => {
   return (
     <div className="testimonial-container">
-      <h2 className="testimonial-heading">Student Testimonials</h2>
+      
 
       {/* Top Grid */}
       <div className="testimonial-grid">
-        <div className="grid-item main">Main</div>
+        <div className="grid-item main">
+          <img src={mainImage} />
+        </div>
           {/* Row 1 */}
-          <div className="grid-item small" style={{ gridColumn: '3' }}></div>
-          <div className="grid-item small" style={{ gridColumn: '4' }}></div>
-          <div className="grid-item small" style={{ gridColumn: '5' }}></div>
+          <div className="grid-item small" style={{ gridColumn: '3' }}>
+            <img src={image1} />
+          </div>
+          <div className="grid-item small" style={{ gridColumn: '4' }}>
+            <img src={image2} />
+          </div>
+          <div className="grid-item small" style={{ gridColumn: '5' }}>
+            <img src={image3} />
+          </div>
 
           {/* Row 2 */}
-          <div className="grid-item small" style={{ gridColumn: '3', gridRow: '2' }}></div>
-          <div className="grid-item small" style={{ gridColumn: '4', gridRow: '2' }}></div>
-          <div className="grid-item small" style={{ gridColumn: '5', gridRow: '2' }}></div>
+          <div className="grid-item small" style={{ gridColumn: '3', gridRow: '2' }}>
+            <img src={image4} />
+          </div>
+          <div className="grid-item small" style={{ gridColumn: '4', gridRow: '2' }}>
+            <img src={image5} />
+          </div>
+          <div className="grid-item small" style={{ gridColumn: '5', gridRow: '2' }}>
+            <img src={image6} />
+          </div>
       </div>
 
-      {/* Jane's Story Section */}
       <div className="story-section">
-        <h3 className="story-title">Jane’s Story</h3>
+        <h3 className="story-title">{name}'s Story</h3>
         <span className="story-body">
-          Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes,
-          or even a very very short story. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Etiam vel magna nec felis fringilla sagittis. Orci varius natoque penatibus et magnis dis
-          parturient montes, nascetur ridiculus mus. Cras placerat magna massa, nec rhoncus urna
-          accumsan eu. Quisque eu risus non diam consequat auctor. Nam tristique venenatis
-          venenatis. Maecenas lobortis nec orci id vestibulum. Vestibulum tellus lectus, vehicula
-          nec quam a, posuere malesuada arcu. Vivamus rutrum dignissim ex eu ultrices. Nulla
-          molestie tincidunt magna aliquam aliquet. Phasellus pretium sem vel lacus ullamcorper
-          sollicitudin. Nunc sit amet diam dui. Praesent semper viverra quam sed pellentesque.
-          Donec maximus nisl ut nisl gravida lobortis.
+          {story}
         </span>
       </div>
     </div>
--- saa-app/src/home/homePage.tsx
+++ saa-app/src/home/homePage.tsx
@@ -62,12 +62,32 @@ function Home() {
                             description='Hello ello'
                             image='TEANSeoul.png' />
                     </div>
+                <h2 className="testimonial-heading">Student Testimonials</h2>
                 <div className = "photo-slideshow">
                     <div className={`slide ${currentSlide === 0 ? "active" : ""}`}>
-                        <StudentTestimonials />
+                        <StudentTestimonials
+                            name = "Timmy"
+                            story = "I loved my time in Sydney so much. We were there whilst Vivid was on which was amazing. We went to the blue mountains and went on lots of hikes" 
+                            mainImage = "SydneyMainPhoto.jpeg"
+                            image1='Sydney1Photo.jpg'
+                            image2='Sydney2Photo.JPG'
+                            image3='Sydney3Photo.jpeg'
+                            image4='Sydney4Photo.jpeg'
+                            image5='Sydney5Photo.jpeg'
+                            image6='Sydney1Photo.jpg'
+                            />
                     </div>
                      <div className={`slide ${currentSlide === 1 ? "active" : ""}`}>
-                        <StudentTestimonials />
+                        <StudentTestimonials 
+                        name = "John"
+                            story = "I loved my time in Sydney so much. We were there whilst Vivid was on which was amazing. We went to the blue mountains and went on lots of hikes" 
+                            mainImage = "SydneyMainPhoto.jpeg"
+                            image1='Sydney1Photo.jpg'
+                            image2='Sydney2Photo.JPG'
+                            image3='Sydney3Photo.jpeg'
+                            image4='Sydney4Photo.jpeg'
+                            image5='Sydney5Photo.jpeg'
+                            image6='Sydney1Photo.jpg'/>
                     </div>
                 </div>    
             </div>
