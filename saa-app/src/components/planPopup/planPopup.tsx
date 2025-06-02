7a2f2d9e59cd97df28665d740547e2f25deca295
Venice Lin <lin.yingw@northeastern.edu>
2025-06-02 10:42:14 +1000

    saved courses popup done

--- saa-app/src/components/planPopup/planPopup.tsx
+++ saa-app/src/components/planPopup/planPopup.tsx
@@ -1,9 +1,7 @@
-import React, {useState} from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import './planPopup.css';
-import CourseCard from "../courseCard/courseCard";
-import Unicard from "../university-card/unicard";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faXmark} from "@fortawesome/free-solid-svg-icons";
+import PopupUniSelect from "../popupUniSelect/popupUniSelect";
 
 interface Course {
     courseName: string;
@@ -17,33 +15,50 @@ interface SavedCoursesInfo {
     credits: number;
     universityName: string;
     savedCourses: Course[];
+    onClose: () => void;
 }
 
-function PlanPopup({ credits, universityName, savedCourses }: SavedCoursesInfo) {
+function PlanPopup({ credits, universityName, savedCourses, onClose }: SavedCoursesInfo) {
+    const handleClose = () => {
+        console.log('Delete button clicked, calling onClose');
+        if (typeof onClose === 'function') {
+            onClose();
+        } else {
+            console.error('onClose is not a function!');
+        }
+    };
+
     return (
         <div className="saved-courses-popup">
-            <div className="delete-button">
-                <FontAwesomeIcon icon={faXmark} />
+            <div>
+                <button
+                    className="delete-button"
+                    onClick={handleClose}
+                    aria-label="Close popup"
+                >
+                    <FontAwesomeIcon icon={faXmark} />
+                </button>
             </div>
-            <h3 className="university-header">{universityName}</h3>
-            <div className="max-credits">Max Credits per Semester: {credits}</div>
-            <div className="courses-list">
+
+            <h3 className="university-header-popup">{universityName}</h3>
+            <div className="max-credits">Max Credits: {credits}</div>
+            <div className="include-in-plan">Included in Plan</div>
+
+            <div className="courses-list" style={{ maxHeight: '330px', overflowY: 'auto' }}>
                 {savedCourses.map((course, index) => (
-                    <>
-                        <Unicard
-                            key={index}
+                    <div key={index}>
+                        <PopupUniSelect
                             courseName={course.courseName}
                             creditAmount={course.creditAmount}
                             location={course.location}
                             nuCourse={course.nuCourse}
                             uniName={course.uniName}/>
-                        <hr/>
-                    </>
+                        {index < savedCourses.length - 1 && <hr className="course-divider" />}
+                    </div>
                 ))}
             </div>
         </div>
     );
 }
 
-//<img src={"/UTS.jpg"} alt="University Photo" />;
 export default PlanPopup;
